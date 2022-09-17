943a99a99a4dbae7ac0e2a9cb83ee6219a0db062
sky-kryst <aakashmeshram@outlook.com>
2022-09-18 00:17:11 +0530

    build(repo): :wrench: site url in astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.sky-kryst.github.io",
+  site: "https://sky-kryst.github.io",
   integrations: [mdx(), sitemap(), solidJs(), tailwind()],
 });
